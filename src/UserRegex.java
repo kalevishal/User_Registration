5896c22fa49ebfa0c6b725d1bfc23566f03de39d
kalevishal <kalevishal1610@gmail.com>
2021-10-05 21:47:24 +0530

    phone number validation

--- src/UserRegex.java
+++ src/UserRegex.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegex {
 	public static void main(String[] args) {
 
-		Scanner Scan = new Scanner(System.in);
+		
 
-		System.out.println("Enter your email");
-		String email = Scan.next();
-		String emailRegex = "^[a-zA-Z0-9+_.]+@[a-zA-Z.-]+$";
-		System.out.println("Is the above email valid : " + email.matches(emailRegex));
+		Scanner scan = new Scanner(System.in);
+        String number= "91 9876543210";
+
+        String numberRegex = "[0-9]{1}[0-9]{1}\\s{0,1}[7-9]{1}[0-9]{9}";
+        System.out.println("Enter your contact number");
+        String contact = scan.nextLine();
+        Pattern pattern = Pattern.compile(numberRegex);     
+        Matcher matcher = pattern.matcher(contact);
+        System.out.println("Is Valid ? : "+matcher.matches());
+
 	}
 
 }
