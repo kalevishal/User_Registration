d4b1f013b7cc8026f4131998409112fd6f21d8d2
kalevishal <kalevishal1610@gmail.com>
2021-10-05 21:55:28 +0530

    password validation rule2

--- src/UserRegex.java
+++ src/UserRegex.java
@@ -6,10 +6,10 @@ public class UserRegex {
 		
 
 		Scanner scan = new Scanner(System.in);
-		 String passwordRegexRule1= "[!\"#$%&'()*+,-./:;<=>?@\\^_`{|}~A-Za-z0-9]{8,15}+";
+		 String passwordRegexRule2= "^(?=.*[A-Z])(?=.*[a-z0-9*.!@$%^&(){}:;<>,.?/~_+-=|]).{8,32}$";
 	        System.out.println("Enter the password with minimum 8 characters!!!");
 	        String password = scan.next();
-	        System.out.println("Is the password valid ? : "+password.matches(passwordRegexRule1));
+	        System.out.println("Is the password valid ? : "+password.matches(passwordRegexRule2));
 
 	}
 
