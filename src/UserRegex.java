db1bafefc00e27aa6c76ae1e4ff0aa5477198dbc
kalevishal <kalevishal1610@gmail.com>
2021-10-05 20:49:27 +0530

    user registration

---
+++ src/UserRegex.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserRegex  {
+	public static void main(String[] args) {
+		
+		Scanner Scan = new Scanner(System.in);
+		
+		String firstName="^[A-Z][a-zA-Z]{2,20}";
+		System.out.println("Enter a name with minimum three alphabets");
+		 String name = Scan.next();
+		System.out.println("Is the input name valid ? : "+name.matches(firstName));
+				        
+	}
+
+}
