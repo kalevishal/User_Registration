2eb07c06918804ccad68b25850c8115a5c02d392
kalevishal <kalevishal1610@gmail.com>
2021-10-05 21:51:01 +0530

    password validation rule1

--- src/UserRegex.java
+++ src/UserRegex.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class UserRegex {
 	public static void main(String[] args) {
@@ -8,14 +6,10 @@ public class UserRegex {
 		
 
 		Scanner scan = new Scanner(System.in);
-        String number= "91 9876543210";
-
-        String numberRegex = "[0-9]{1}[0-9]{1}\\s{0,1}[7-9]{1}[0-9]{9}";
-        System.out.println("Enter your contact number");
-        String contact = scan.nextLine();
-        Pattern pattern = Pattern.compile(numberRegex);     
-        Matcher matcher = pattern.matcher(contact);
-        System.out.println("Is Valid ? : "+matcher.matches());
+		 String passwordRegexRule1= "[!\"#$%&'()*+,-./:;<=>?@\\^_`{|}~A-Za-z0-9]{8,15}+";
+	        System.out.println("Enter the password with minimum 8 characters!!!");
+	        String password = scan.next();
+	        System.out.println("Is the password valid ? : "+password.matches(passwordRegexRule1));
 
 	}
 
