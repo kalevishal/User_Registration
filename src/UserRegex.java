c7433fd84558fbcd5c61fc86383d3003085298d2
kalevishal <kalevishal1610@gmail.com>
2021-10-05 21:41:51 +0530

    email validation

--- src/UserRegex.java
+++ src/UserRegex.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class UserRegex {
 	public static void main(String[] args) {
-		
+
 		Scanner Scan = new Scanner(System.in);
-		
-		String lastName="^[A-Z][a-zA-Z]{2,20}";
-		System.out.println("Enter a name with minimum three alphabets");
-		 String name = Scan.next();
-		System.out.println("Is the input name valid ? : "+name.matches(lastName));
-				        
+
+		System.out.println("Enter your email");
+		String email = Scan.next();
+		String emailRegex = "^[a-zA-Z0-9+_.]+@[a-zA-Z.-]+$";
+		System.out.println("Is the above email valid : " + email.matches(emailRegex));
 	}
 
 }
